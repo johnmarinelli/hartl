824362460ab248c7dfcd53135c9c1e0a7d7fc5ff
John Marinelli <john.marinelli.dev@gmail.com>
2017-07-20 20:24:52 -0700

    add validation for user and microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :email, length: { minimum: 5, maximum: 140 }, presence: true
+  validates :name, length: { minimum: 5, maximum: 20 }, presence: true
   has_many :microposts
 end
