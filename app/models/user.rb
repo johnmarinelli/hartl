71b69592d79faed460d2e0868df83fb9a50b4e1e
John Marinelli <john.marinelli.dev@gmail.com>
2017-07-21 09:54:55 -0700

    finish chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-  validates :email, length: { minimum: 5, maximum: 140 }, presence: true
+  before_save { self.email = email.downcase }
   validates :name, length: { minimum: 5, maximum: 20 }, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { minimum: 5, maximum: 140 }, 
+                    presence: true, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, presence: true
   has_many :microposts
+
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,66 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(name: 'user3', email: 'email@hotmail.com', password: 'barfoo', password_confirmation: 'barfoo')
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "             "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "             "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = 'a' * 500
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = 'a' * 500 + '@hotmail.com'
+    assert_not @user.valid?
+  end
+
+  test "email is valid" do
+    @user.email = 'abcdefnot@email,com'
+    assert_not @user.valid?
+  end
+
+  test "emails should be unique" do
+    dupe = @user.dup
+    @user.save
+    assert_not dupe.valid?
+  end
+
+  test "emails should be unique and case insensitive" do
+    dupe = @user.dup
+    dupe.email = @user.email.upcase
+    @user.save
+    assert_not dupe.valid?
+  end
+
+  test "emails are saved as lowercase" do
+    new_email = "foOOoO@eXampLe.CoM"
+    @user.email = new_email
+    @user.save
+    assert_equal new_email.downcase, @user.email
+  end
+
+  test "passwords should be non blank" do
+    @user.password = @user.password_confirmation = ' '
+    assert_not @user.valid?
+  end
+
+  test "passwords have minimum length of 6" do
+    @user.password = @user.password_confirmation = 's'
+    assert_not @user.valid?
+  end
 end
