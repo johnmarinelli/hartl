734a1ab07a806b77b13dfc68aeb673f43ab8d1f4
John Marinelli <john.marinelli.dev@gmail.com>
2017-07-24 11:50:17 -0700

    advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,4 +35,36 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_redirected_to root_url
   end
+
+  test "login with valid information followed by logout" do
+    @user = users :one
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email.downcase, password: 'password1' } }
+    assert_redirected_to @user
+    follow_redirect!
+
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    @user = users :one
+    log_in_as @user, password: 'password1', remember_me: '1'
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    @user = users :one
+    log_in_as @user, password: 'password1', remember_me: '1'
+    log_in_as @user, password: 'password1', remember_me: '0'
+    assert_empty cookies['remember_token']
+  end
 end
