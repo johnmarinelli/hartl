57694ba11bf003d34d0d26f37f8ac8ba3de0ad7e
John Marinelli <john.marinelli.dev@gmail.com>
2017-07-24 17:49:00 -0700

    add test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users :one
+    @other_user = users :two
   end
 
   test "index including pagination" do
@@ -19,5 +20,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   test "unauthenticated user can't see user index" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "non admin user can't see delete link" do
+    log_in_as @other_user, 'password2'
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
